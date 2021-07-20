5e0136a10c70622134f23251344a882d91259304
Hydroxion <whenrique26@gmail.com>
2021-07-20 13:55:59 -0300

    Remove text correction

--- main.py
+++ main.py
@@ -2,22 +2,10 @@ import easyocr
 
 from google_trans_new import google_translator
 
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-import torch
-
 import cv2
 
 
 translator = google_translator()
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-tokenizer = AutoTokenizer.from_pretrained('salesken/grammar_correction')
-
-model = AutoModelForCausalLM.from_pretrained(
-    'salesken/grammar_correction'
-).to(device)
 
 
 def ocr(image, language_origin, paragraph):
@@ -34,26 +22,8 @@ def translate(text, language_origin, language_target):
     )
 
 
-def grammar_correction(text):
-    query = "<|startoftext|> " + text  # '~~~'
-
-    input_ids = tokenizer.encode(query.lower(), return_tensors='pt').to(device)
-
-    sample_output = model.generate(
-        input_ids,
-        do_sample=True,
-        num_beams=1,
-        max_length=128,
-        temperature=0.9,
-        top_p=0.85,
-        top_k=5,
-        num_return_sequences=1
-    )
-
-    return tokenizer.decode(sample_output[0], skip_special_tokens=True).split('||')[0].split('~~~')[1]
-
-
 def photoshop(image, image_ocr, language_origin, paragraph, language_target):
+    # Revert paint to avoid overlap
     for sentence in reversed(image_ocr):
         boxes, text, confident = [*sentence, None] if paragraph else sentence
 
@@ -72,8 +42,6 @@ def photoshop(image, image_ocr, language_origin, paragraph, language_target):
             if isinstance(translate_text, str) else
             translate_text[0]
         ).capitalize()
-
-        print(box_text)
 
         cv2.putText(
             image,
